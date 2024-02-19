8406db0242671ed770645bc2be02747f807a5e5c
Csizmadia Márk <airamek@teamorange.hu>
2024-02-19 14:06:36 +0100

    Add project files.

---
+++ SZGYA-Legszennyezes-20240219/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace SZGYA_Legszennyezes_20240219
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, List<int>> legadatok = new();
+
+            var sr = new StreamReader("../../../src/SO2.txt", Encoding.UTF8);
+
+            for (int i = 1; !sr.EndOfStream; i++)
+            {
+                legadatok.Add(i, sr.ReadLine().Split(';').Select(l => int.Parse(l)).ToList());
+            }
+
+            //2
+            Console.WriteLine("\n2.feladat");
+            foreach (var l in legadatok)
+            {
+                Console.WriteLine(string.Join(' ', l.Value));
+            }
+
+            //3
+            Console.WriteLine("\n3.feladat");
+            foreach (var l in legadatok)
+            {
+                Console.WriteLine(l.Value.FirstOrDefault(n => n > 250));
+            }
+
+        }
+    }
+}
