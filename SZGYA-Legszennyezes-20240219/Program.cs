a68b9136320a1f0dd964ed82e0a2634d728620ef
Csizmadia Márk <airamek@teamorange.hu>
2024-02-20 09:29:56 +0100

    sima feladatok kész

--- SZGYA-Legszennyezes-20240219/Program.cs
+++ SZGYA-Legszennyezes-20240219/Program.cs
@@ -23,11 +23,27 @@ namespace SZGYA_Legszennyezes_20240219
             }
 
             //3
+            var sw = new StreamWriter("../../../src/datum.txt", false, Encoding.UTF8);
             Console.WriteLine("\n3.feladat");
             foreach (var l in legadatok)
             {
-                Console.WriteLine(l.Value.FirstOrDefault(n => n > 250));
+                if (l.Value.Any(n => n > 250)) sw.WriteLine(DateTime.Parse($"3.{l.Key}.").ToString("MMMM d."));
             }
+            sw.Close();
+
+            //4
+            var max = legadatok.MaxBy(l => l.Value.Max());
+            Console.WriteLine($"\n4.feladat: {max.Key}. nap {max.Value.IndexOf(max.Value.Max()) + 1} óra");
+
+            //5
+            Console.WriteLine($"\n5.feladat: {legadatok.Sum(l => l.Value.Count(n => n < 100))}");
+
+            //6
+            Console.WriteLine($"\n6.feladat: {legadatok.Average(l => l.Value.Average())}");
+
+            //7
+            var hatvanalatt = legadatok.FirstOrDefault(l => l.Value.All(n => n < 60));
+            Console.WriteLine($"\n7.feladat \n\t{(hatvanalatt.Key != 0 ? $"{hatvanalatt.Key}" : "[HIBA] Nincs ilyen nap")}");
 
         }
     }
